2f7ae45295ef104857d68683f90d52a11279a75b
L-I-V-15 <liv.hahndorf@gmx.de>
2026-04-09 16:46:14 +0200

    konsole test 2

--- helloworld/src/helloworld/Main.java
+++ helloworld/src/helloworld/Main.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello World! Linn");
+        System.out.println("Hello World! Liv test");
     }
 }
