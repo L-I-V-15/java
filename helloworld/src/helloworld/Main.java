d7460707d67e3b76bbfeba09aad875bc29364f0e
L-I-V-15 <liv.hahndorf@gmx.de>
2026-04-09 16:51:14 +0200

    konsole nur master

--- helloworld/src/helloworld/Main.java
+++ helloworld/src/helloworld/Main.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello World! Liv test");
+        System.out.println("Hello World! Liv nur master");
     }
 }
