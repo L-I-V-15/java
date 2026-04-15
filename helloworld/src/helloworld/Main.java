b290dc0faeab2e397795bcf85fc45193b565da6f
L-I-V-15 <liv.hahndorf@gmx.de>
2026-04-15 14:22:56 +0200

    test sadf

--- helloworld/src/helloworld/Main.java
+++ helloworld/src/helloworld/Main.java
@@ -2,6 +2,6 @@ package helloworld;
 
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello World! Liv nur master 2");
+        System.out.println("Hello World! Liv nur master 2 jbkjb");
     }
 }
